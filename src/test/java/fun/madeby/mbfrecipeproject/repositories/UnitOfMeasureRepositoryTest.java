499787425671227d6c32b47ef5bdfbddd9303e49
gra-m <91182298+gra-m@users.noreply.github.com>
2022-04-13 07:37:08 +0200

    closes #21 IntegrationTest Junit4 findUnitOfMeasureByDescription

---
+++ src/test/java/fun/madeby/mbfrecipeproject/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,52 @@
+package fun.madeby.mbfrecipeproject.repositories;
+
+import fun.madeby.mbfrecipeproject.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class) //Junit4
+@DataJpaTest
+class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DirtiesContext
+    void findUnitOfMeasureHandful() {
+        //given
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findUnitOfMeasureByDescription("handful");
+
+        //when
+        String expectedValue = "handful";
+
+        //then
+        assertEquals(expectedValue, unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    void findUnitOfMeasureCup() {
+        //given
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findUnitOfMeasureByDescription("cup");
+
+        //when
+        String expectedValue = "cup";
+
+        //then
+        assertEquals(expectedValue, unitOfMeasureOptional.get().getDescription());
+    }
+}
