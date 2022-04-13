00642057b290899aeaae6ad3ac4b9068516b27ce
gra-m <91182298+gra-m@users.noreply.github.com>
2022-04-13 13:25:58 +0200

    closes #24 UnitOfMeasureRepositoryIT updated to J5

--- src/test/java/fun/madeby/mbfrecipeproject/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/fun/madeby/mbfrecipeproject/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -3,18 +3,22 @@ package fun.madeby.mbfrecipeproject.repositories;
 import fun.madeby.mbfrecipeproject.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class) //Junit4
-@DataJpaTest
+//@DataJpaTest Runs, but noticeably faster with @SpringBootTest 383ms vs 192ms
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class UnitOfMeasureRepositoryTestIT {
 
     @Autowired
