488da93bbfb3f8be0e4446e72b14b194a83d5929
gra-m <91182298+gra-m@users.noreply.github.com>
2022-04-13 09:59:02 +0200

    closes #22 mvn verify first runs unit tests, then more expensive integration testsIT

--- src/test/java/fun/madeby/mbfrecipeproject/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/fun/madeby/mbfrecipeproject/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -3,7 +3,6 @@ package fun.madeby.mbfrecipeproject.repositories;
 import fun.madeby.mbfrecipeproject.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
