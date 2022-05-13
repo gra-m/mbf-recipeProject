3da019cd3f27fd7c9058a2c76b50977de49fc423
gra-m <91182298+gra-m@users.noreply.github.com>
2022-05-13 15:42:57 +0200

    #100 playing around with mysql docker along with yt vids, didn't get there, not sure I want to at present so that's enough for now

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,14 +1,18 @@
 ## Use the below to run mysql docker image instead of local mysql db.
-#docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+#docker run --name recipe1 -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0
 
 # connect to mysql and run as root users
 #Create Database Schemas
 CREATE DATABASE mbf_dev;
 CREATE DATABASE mbf_prod;
 
-#Create database service accounts (No Root access)
+#Create database service accounts (No Root access)Local
 CREATE USER 'mbf_dev_user'@'localhost' IDENTIFIED BY 'gram';
 CREATE USER 'mbf_prod_user'@'localhost' IDENTIFIED BY 'gram';
+
+#Create database service accounts (No Root access) Docker
+CREATE USER 'mbf_dev_user'@'127.0.0.1' IDENTIFIED BY 'gram';
+CREATE USER 'mbf_prod_user'@'127.0.0.1' IDENTIFIED BY 'gram';
 
 #Database grants for local mysql
 GRANT SELECT ON mbf_dev.* to 'mbf_dev_user'@'localhost';
@@ -20,11 +24,11 @@ GRANT INSERT ON mbf_prod.* to 'mbf_prod_user'@'localhost';
 GRANT DELETE ON mbf_prod.* to 'mbf_prod_user'@'localhost';
 GRANT UPDATE ON mbf_prod.* to 'mbf_prod_user'@'localhost';
 #Database grants for docker mysql
-GRANT SELECT ON mbf_dev.* to 'mbf_dev_user'@'%';
-GRANT INSERT ON mbf_dev.* to 'mbf_dev_user'@'%';
-GRANT DELETE ON mbf_dev.* to 'mbf_dev_user'@'%';
-GRANT UPDATE ON mbf_dev.* to 'mbf_dev_user'@'%';
-GRANT SELECT ON mbf_prod.* to 'mbf_prod_user'@'%';
-GRANT INSERT ON mbf_prod.* to 'mbf_prod_user'@'%';
-GRANT DELETE ON mbf_prod.* to 'mbf_prod_user'@'%';
-GRANT UPDATE ON mbf_prod.* to 'mbf_prod_user'@'%';
+GRANT SELECT ON mbf_dev.* to 'mbf_dev_user'@'127.0.0.1';
+GRANT INSERT ON mbf_dev.* to 'mbf_dev_user'@'127.0.0.1';
+GRANT DELETE ON mbf_dev.* to 'mbf_dev_user'@'127.0.0.1';
+GRANT UPDATE ON mbf_dev.* to 'mbf_dev_user'@'127.0.0.1';
+GRANT SELECT ON mbf_prod.* to 'mbf_prod_user'@'127.0.0.1';
+GRANT INSERT ON mbf_prod.* to 'mbf_prod_user'@'127.0.0.1';
+GRANT DELETE ON mbf_prod.* to 'mbf_prod_user'@'127.0.0.1';
+GRANT UPDATE ON mbf_prod.* to 'mbf_prod_user'@'127.0.0.1';
